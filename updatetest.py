abe062797ff5e6e77bd289005c4188c5eafda1f9
Boydsquirrel <cross.boyd@icloud.com>
2025-12-01 21:45:10 +0000

    Add version checking to update test script

--- updatetest.py
+++ updatetest.py
@@ -1,1 +1,20 @@
-this is v1
+import time
+import os
+
+VERSION_FILE = "version.txt"
+
+def get_local_version():
+    if VERSION_FILE not in os.listdir():
+        return "0.0"
+    with open(VERSION_FILE, "r") as f:
+        return f.read().strip()
+
+def main():
+    ver = get_local_version()
+    print("=== Update Test Script Running ===")
+    print("Version installed:", ver)
+    while True:
+        print("Running version:", ver)
+        time.sleep(1)
+
+main()
