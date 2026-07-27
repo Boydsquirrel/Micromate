fa42baca06c954758b372b9a1d2fd5c20fca1c94
Boydsquirrel <cross.boyd@icloud.com>
2026-07-27 16:54:35 +0100

    Refactor button handling with new button names

--- buttons.py
+++ buttons.py
@@ -2,40 +2,53 @@
 import time
 from machine import Pin
 
-button1 = Pin(17, Pin.IN, Pin.PULL_UP)
-button2 = Pin(19, Pin.IN, Pin.PULL_UP)
-button3 = Pin(18, Pin.IN, Pin.PULL_UP)
-button4 = Pin(26, Pin.IN, Pin.PULL_UP)
+right = Pin(17, Pin.IN, Pin.PULL_UP)
+left = Pin(19, Pin.IN, Pin.PULL_UP)
+select = Pin(18, Pin.IN, Pin.PULL_UP)
+alt_b = Pin(26, Pin.IN, Pin.PULL_UP)
+down = Pin(4, Pin.IN, Pin.PULL_UP)
+up = Pin(16, Pin.IN, Pin.PULL_UP)
 
 def button_input():
     button_pressed = 0
-    if button1.value() == 0:  
-        print("Button 1 Pressed!")
+    if right.value() == 0:  
+        print("right")
         button_pressed = 1
-        while button1.value() == 0:
+        while right.value() == 0:
             time.sleep(0.01)
         time.sleep(0.05)
 
-    elif button2.value() == 0:
-        print("Button 2 Pressed!")
+    elif left.value() == 0:
+        print("left")
         button_pressed = 2
-        while button2.value() == 0:
+        while left.value() == 0:
             time.sleep(0.01)
         time.sleep(0.05)
 
-    elif button3.value() == 0:
-        print("Button 3 Pressed!")
+    elif select.value() == 0:
+        print("select")
         button_pressed = 3
-        while button3.value() == 0:
+        while select.value() == 0:
             time.sleep(0.01)
         time.sleep(0.05)
 
-    elif button4.value() == 0:
-        print("Button 4 Pressed!")
+    elif alt_b.value() == 0:
+        print("alternate button")
         button_pressed = 4
-        while button4.value() == 0:
+        while alt_b.value() == 0:
             time.sleep(0.01)
         time.sleep(0.05)
-    
+    elif down.value() == 0:
+        print("down")
+        button_pressed = 4
+        while down.value() == 0:
+            time.sleep(0.01)
+        time.sleep(0.05)
+    elif up.value() == 0:
+        print("up")
+        button_pressed = 4
+        while up.value() == 0:
+            time.sleep(0.01)
+        time.sleep(0.05)
     return button_pressed
 
